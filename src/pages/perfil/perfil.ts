38c420e5634a6869684bdc4a1524cb13aecec4d5
Edi P. Silva <edipaulinodasilva@gmail.com>
2017-10-21 17:21:21 -0200

    Conclução perfil

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,12 +1,6 @@
+import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the PerfilPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +9,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PerfilPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	public usuario = JSON.parse(localStorage.getItem('usuario'));
+	public fones:string = '';
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PerfilPage');
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+	}
+
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad PerfilPage');
+		this.usuario[0].sexo = this.usuario[0].sexo == 2 ? 'Feminio' : 'Masculino';
+		this.fones = this.usuario[0].telefone == true ? this.usuario[0].telefone+' / '+this.usuario[0].celular : this.usuario[0].celular;
+		this.usuario[0].doador = this.usuario[0].doador == 0 ? 'Não' : 'Sim';
+		this.usuario[0].tipo_sangue = this.usuario[0].tipo_sangue == null ? 'Não cadastrado' : this.usuario[0].tipo_sangue;
+		this.usuario[0].email = this.usuario[0].email == null ? 'Não cadastrado' : this.usuario[0].email;
+	}
+
+	voltar(){
+		this.navCtrl.setRoot(HomePage);
+	}
 
 }
