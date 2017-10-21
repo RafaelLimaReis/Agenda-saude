a73be866b2232210a1652259d286197330e810f1
dieniffersabrina <dieniffer@live.com>
2017-10-21 12:36:51 -0200

    correção de bugs no menu e alterações

---
+++ src/pages/apoio/apoio.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ApoioPage } from './apoio';
+
+@NgModule({
+  declarations: [
+    ApoioPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ApoioPage),
+  ],
+})
+export class ApoioPageModule {}
