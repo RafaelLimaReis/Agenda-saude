07c9719e3dde88521d2921a78a6cfae79a205919
RafaelLimaReis <rafaellimareis01@gmail.com>
2017-11-07 20:06:51 -0200

    Implementação de 'type network' para verificar tipo de conexão

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,3 +1,4 @@
+import { Network } from '@ionic-native/network';
 import { Login_2Page } from './../login-2/login-2';
 import { apiPrefeitura } from './../../services/api-prefeitura';
 import { Component } from '@angular/core';
@@ -10,18 +11,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  private auth: FormGroup;
+  private auth: FormGroup; //FormGroup para validar campos obrigatorios
 
   public usuario: any = {};
 
   constructor(public navCtrl: NavController, public navParams: NavParams, 
               public apiPrefeitura: apiPrefeitura, public alertCtrl: AlertController,
               public loadController: LoadingController, private Form:FormBuilder,
-              public menuCTRL: MenuController) {
+              public menuCTRL: MenuController, private network:Network) {
+    //validação de dados            
     this.auth = this.Form.group({
       cartao: ['', Validators.compose([Validators.required])] 
     });
-
+    //bloquear menu
     this.menuCTRL.enable(false);
 
     this.consultaUser();
@@ -32,36 +34,53 @@ export class LoginPage {
   }
 
   authenticate(){
-    const loading = this.loadController.create({content:'Aguarde...'});
-    loading.present(loading);
-    
-    let cartao = this.auth.value.cartao;
-    this.apiPrefeitura.auth(cartao).subscribe(res => {
-      loading.dismiss();
+    let connection = this.network.type; //pegar tipo de conexão atual do aparelho
 
-      let name = res.data[0].nome.split(' ');
-      res.data[0].nome = name[0] + ' ' + name[name.length - 2];
-      res.data[0].flag = false;
-      localStorage.setItem('usuario',JSON.stringify(res.data));
-      this.navCtrl.push(Login_2Page);
-    }, err => {
-      loading.dismiss();
-      if (err.status === 408) {
-        let alert = this.alertCtrl.create({
-          title: 'Ocorreu um erro!',
-          message: 'Ocorreu algum problema. Tente mais tarde.',
-          buttons: ['Voltar']
-        });
-        alert.present();
-      } else if (err.status === 404) {
-        let alert = this.alertCtrl.create({
-          title: 'Ocorreu um erro!',
-          message: 'Não encontramos seus dados.',
-          buttons: ['Voltar']
-        });
-        alert.present();
-      }
-    });
+    if ((connection === 'none') || (connection === 'unknown') || (connection === 'ethernet')){
+      //setando alert
+      let alert = this.alertCtrl.create({
+        title: 'Ocorreu um erro!',
+        message: 'Conecte na internet para fazer o login.',
+        buttons: ['Voltar']
+      });
+      alert.present(); //lançando alert
+
+    } else {
+
+      const loading = this.loadController.create({content:'Aguarde...'}); // new modal 'carregando'
+      loading.present(loading); // lançando modal
+      
+      let cartao = this.auth.value.cartao;
+
+      this.apiPrefeitura.auth(cartao).subscribe(res => {
+        loading.dismiss(); // eliminando modal
+
+        let name = res.data[0].nome.split(' '); // cortando nome
+        res.data[0].nome = name[0] + ' ' + name[name.length - 1]; //juntando nome e sobrenome
+        res.data[0].flag = false; // setando flag para validar 2 fase login
+
+        localStorage.setItem('usuario',JSON.stringify(res.data)); //salvando dados usuario
+        this.navCtrl.push(Login_2Page); // lançando 2 fase login
+      }, err => {
+        loading.dismiss();
+        // 'bad request' nenhum dado encontrado
+        if (err.status === 404) {
+          let alert = this.alertCtrl.create({
+            title: 'Ocorreu um erro!',
+            message: 'Não encontramos seus dados.',
+            buttons: ['Voltar']
+          });
+          alert.present();
+        } else {
+          let alert = this.alertCtrl.create({
+            title: 'Ocorreu um erro!',
+            message: 'Ocorreu algum problema. Tente mais tarde.',
+            buttons: ['Voltar']
+          });
+          alert.present();
+        }
+      });
+    }
   }
 
   consultaUser(){
